c5364bc10c1ebfe3fe8cd82fa0228655e93e900a
Lavda <kaustubhwahane55@gmail.com>
2023-12-10 23:52:29 +0530

    Template done

---
+++ src/component/Data.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import worldLogo from '../Images/wordLogo.jpeg';
+
+function Data() {
+  
+  return (
+    <div className='outerborder'>
+        <nav>
+            <img src={worldLogo} alt='Error 404' className="world" /><p>My Travel Journals</p>
+        </nav>
+    </div>
+  )
+}
+
+export default Data;
---
+++ src/component/Locations.js
@@ -0,0 +1,26 @@
+const locations = [
+    {
+      id: 1,
+      title: 'Mount Fuji',
+      startDate: '2023-12-15T10:00:00Z',
+      endDate: '2023-12-18T18:00:00Z',
+      locationDescription: 'Mount Fuji is an iconic stratovolcano in Japan, standing at 3,776 meters (12,389 feet). It is not only a symbol of Japan but also a UNESCO World Heritage Site.',
+    },
+    {
+      id: 2,
+      title: 'Sydney Opera House',
+      startDate: '2023-12-20T15:30:00Z',
+      endDate: '2023-12-23T20:45:00Z',
+      locationDescription: 'The Sydney Opera House is a masterpiece of modern architecture. Located in Sydney, Australia, it is a multi-venue performing arts center and an iconic symbol of the city.',
+    },
+    {
+      id: 3,
+      title: 'Geirangerfjord',
+      startDate: '2023-12-25T08:00:00Z',
+      endDate: '2023-12-28T16:30:00Z',
+      locationDescription: 'Geirangerfjord is a breathtaking fjord in Norway, known for its stunning landscapes, waterfalls, and picturesque villages. It is a UNESCO World Heritage Site and a popular destination for nature lovers.',
+    },
+  ];
+  
+  export default locations;
+  
