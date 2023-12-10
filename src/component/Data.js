d9fd0e05837e3c4e2c38e83b865c3b47e78a4a46
Lavda <kaustubhwahane55@gmail.com>
2023-12-11 00:13:28 +0530

    Template Done

--- src/component/Data.js
+++ src/component/Data.js
@@ -8,6 +8,13 @@ function Data() {
         <nav>
             <img src={worldLogo} alt='Error 404' className="world" /><p>My Travel Journals</p>
         </nav>
+        <div className="insideBox">
+          <img src='' alt='Error 4069' className='insideImage'/>
+          <h3 className='title'>Hello</h3>
+          <p className='start'>Start Date</p>
+          <p className='end'>End Date</p>
+          <p className='Description'>Lorem15</p>
+        </div>
     </div>
   )
 }
