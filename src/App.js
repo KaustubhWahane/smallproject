133c0c3757f7a1a7f1cc31ff9efadc964ade4f96
Lavda <kaustubhwahane55@gmail.com>
2023-12-11 16:24:34 +0530

    Problem with Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
 import Data from './component/Data';
+import MountFuji from '../src/Images/MountFuji.jpg'
+import info from './component/Locations';
 
 function App() {
   return (
     <div className="App">
-     <Data/>
+      {info.map((location) => (
+        <Data
+          key={location.id}
+          title={location.title}
+          startDate={location.startDate}
+          endDate={location.endDate}
+          locationDescription={location.locationDescription}
+          image={MountFuji} // You might want to make this dynamic based on location
+        />
+      ))}
     </div>
   );
+  // return (
+  //   <div className="App">
+  //      <Data
+  //       title= {'Mount Fuji'}
+  //       startDate = {'2023-12-15'}
+  //       endDate ={'2023-12-18'}
+  //       locationDescription = {'Mount Fuji is an iconic stratovolcano in Japan, standing at 3,776 meters (12,389 feet). It is not only a symbol of Japan but also a UNESCO World Heritage Site.'}
+  //       image ={MountFuji}
+  //      />
+  //   </div>
+  // );
 }
 
 export default App;
--- src/component/Data.js
+++ src/component/Data.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import worldLogo from '../Images/wordLogo.jpeg';
 
-function Data() {
-  
+function Data(props) {
   return (
     <div className='outerborder'>
         <nav>
             <img src={worldLogo} alt='Error 404' className="world" /><p>My Travel Journals</p>
         </nav>
         <div className="insideBox">
-          <img src='' alt='Error 4069' className='insideImage'/>
-          <h3 className='title'>Hello</h3>
-          <p className='start'>Start Date</p>
-          <p className='end'>End Date</p>
-          <p className='Description'>Lorem15</p>
+          <img src={props.image} alt='Error 4069' className='insideImage'/>
+          <h3 className='title'> {props.title}</h3>
+          <p className='start'>{props.startDate}</p>
+          <p className='end'>{props.endDate}</p>
+          <p className='Description'>{props.locationDescription}</p>
         </div>
     </div>
   )
