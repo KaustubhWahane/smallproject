31105f807b5aa15180bab37620a1b97b4c0a899c
Lavda <kaustubhwahane55@gmail.com>
2023-12-13 14:54:15 +0530

    Error I have to solve

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import './App.css';
 import Data from './component/Data';
 import MountFuji from '../src/Images/MountFuji.jpg'
-import Sydney from '../src/Images/Sydney.jpg'
+import info from './component/Locations';
 
 function App() {
   return (
     <div className="App">
-       <Data
-        title= {'Mount Fuji'}
-        startDate = {'2023-12-15'}
-        endDate ={'2023-12-18'}
-        locationDescription = {'Mount Fuji is an iconic stratovolcano in Japan, standing at 3,776 meters (12,389 feet). It is not only a symbol of Japan but also a UNESCO World Heritage Site.'}
-        image ={MountFuji}
-       />
-       <Data
-        title= {'Sydney Opera House'}
-        startDate = {'2023-12-20'}
-        endDate ={'2023-12-23'}
-        locationDescription = {'The Sydney Opera House is a masterpiece of modern architecture. Located in Sydney, Australia, it is a multi-venue performing arts center and an iconic symbol of the city.'}
-        image ={Sydney}
-       />
-       <Data
-        title= {'Geirangerfjord'}
-        startDate = {'2023-12-25'}
-        endDate ={'2023-12-28'}
-        locationDescription = {'Geirangerfjord is a breathtaking fjord in Norway, known for its stunning landscapes, waterfalls, and picturesque villages. It is a UNESCO World Heritage Site and a popular destination for nature lovers.'}
-        image ={MountFuji}
-       />
+      {info.map((location) => (
+        <Data
+          key={location.id}
+          title={location.title}
+          startDate={location.startDate}
+          endDate={location.endDate}
+          locationDescription={location.locationDescription}
+          image={MountFuji} 
+        />
+      ))}
     </div>
   );
+  //This is just to test one Card
+  // return (
+  //   <div className="App">
+  //      <Data
+  //       title= {'Mount Fuji'}
+  //       startDate = {'2023-12-15'}
+  //       endDate ={'2023-12-18'}
+  //       locationDescription = {'Mount Fuji is an iconic stratovolcano in Japan, standing at 3,776 meters (12,389 feet). It is not only a symbol of Japan but also a UNESCO World Heritage Site.'}
+  //       image ={MountFuji}
+  //      />
+  //   </div>
+  // );
 }
 
 export default App;
--- src/component/Data.js
+++ src/component/Data.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import worldLogo from '../Images/wordLogo.jpeg';
 
 function Data(props) {
   return (
-       
+    <div className='outerborder'>
+        <nav>
+            <img src={worldLogo} alt='Error 404' className="world" /><p>My Travel Journals</p>
+        </nav>
         <div className="insideBox">
           <img src={props.image} alt='Error 4069' className='insideImage'/>
           <h3 className='title'> {props.title}</h3>
@@ -10,6 +14,7 @@ function Data(props) {
           <p className='end'>{props.endDate}</p>
           <p className='Description'>{props.locationDescription}</p>
         </div>
+    </div>
   )
 }
 
