8b801fcff16bf6f7c2dcec27c45c8ecfc378d1ee
Lavda <kaustubhwahane55@gmail.com>
2023-12-13 14:45:37 +0530

    Done

--- src/component/Data.js
+++ src/component/Data.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import worldLogo from '../Images/wordLogo.jpeg';
 
 function Data(props) {
   return (
-    <div className='outerborder'>
-        <nav>
-            <img src={worldLogo} alt='Error 404' className="world" /><p>My Travel Journals</p>
-        </nav>
+       
         <div className="insideBox">
           <img src={props.image} alt='Error 4069' className='insideImage'/>
           <h3 className='title'> {props.title}</h3>
@@ -14,7 +10,6 @@ function Data(props) {
           <p className='end'>{props.endDate}</p>
           <p className='Description'>{props.locationDescription}</p>
         </div>
-    </div>
   )
 }
 
